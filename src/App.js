2b7e324778b08bd6ad9d586b3350c372b2e0c90f
Ajith-Kumar0408 <ajithak0093@gmail.com>
2023-02-23 23:48:20 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faTwitter, faFacebook } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+  <Body/>
+);
+}
+
+function Body(){
+  return(<>
+ <div className='box'>
+  <div className='container'>
+    <div className='smallbox'>
+      <div className='smallbox-1'>
+         <img src="https://avatars2.githubusercontent.com/u/58701512?s=280&v=4" classname="logo"></img>
+       </div>
+      <div className='smallbox-2'>
+       <h3>Sign up on start Bootstrap</h3>
+       <p>Join our community of designers and developers and manage your purchases, favorite<br></br>themes and snippets, comments, and more!</p>
+      </div>
+      <div className='smallbox-3'>
+        <button className='button-1'>Log in with Twitter</button>
+        <button className='button-2'>Log in with Facebook</button>
+        <button className='button-3'>Log in with Google</button>
+        <button className='button-4'>Log in with Github</button>
+      </div>
+      <div className='smallbox-4'>
+        <h4>Email Address</h4>
+        <input className='email-box' required></input>
+        <h4>Password</h4>
+        <input className='email-box' required></input>
+        <h3>Forgot your password</h3>
+        <button className='button-5'>Login</button>
+      </div>
     </div>
+  </div>
+ </div>
+  </>
   );
 }
 
